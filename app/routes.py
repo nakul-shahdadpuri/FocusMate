dba21d7a08769f56827c369650f36c265993f297
Nakul Shahdadpuri <nakulshahdadpuri3141@gmail.com>
2025-03-28 13:06:33 -0400

    beta api complete

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,12 @@
 from flask import Blueprint, request, jsonify
 from services.document_loader import load_documents
-from models.embedder import load_vector_store, create_vector_store, search
+from models.embedder import load_vector_store, search
 from services.answer_generator import generate_answer
-import os
-from config.settings import VECTOR_INDEX
 
 main = Blueprint('main', __name__)
 
 # Load documents and sources
 documents, sources = load_documents()
-
-# Check if the FAISS index file exists, and create it if not
-if not os.path.exists(VECTOR_INDEX):
-    print("Vector index not found. Creating a new one...")
-    create_vector_store(documents)
-
-# Load the vector index
 index = load_vector_store()
 
 @main.route('/ask', methods=['POST'])
--- services/answer_generator.py
+++ services/answer_generator.py
@@ -3,8 +3,12 @@ import ollama
 def generate_answer(context, question):
     prompt = f"Context: {context}\nQuestion: {question}\nAnswer:"
     response = ollama.generate(model="llama3.2:1b", prompt=prompt)
-
+    print(response)
+    # Ensure result aggregation
     result = ""
     for chunk in response:
-        result += chunk["text"]
+        if chunk[0] == "response":
+            result = chunk[1]
+    
+
     return result
